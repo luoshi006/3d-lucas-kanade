41d76f90b06d829a9beef2feb614769bff50cdbc
patricksnape <pts08@ic.ac.uk>
2013-01-28 19:16:03 +0000

    Use meshgrid!

--- face_recognition_2011/tools/jacobian_3d_a.m
+++ face_recognition_2011/tools/jacobian_3d_a.m
@@ -1,13 +1,11 @@
-function dW_dp = jacobian_3d_a(nx, ny, nz);
+function dW_dp = jacobian_3d_a(nx, ny, nz)
 % JACOBIAN_A - Compute Jacobian for affine warp
 %   DW_DP = JACOBIAN_A(WIDTH, HEIGHT, DEPTH)
 
 % Iain Matthews, Simon Baker, Carnegie Mellon University, Pittsburgh
 % $Id: jacobian_a.m,v 1.1.1.1 2003/08/20 03:07:36 iainm Exp $
 
-jac_x = repmat(kron((0:nx - 1), ones(ny, 1)), [1 1 nz]);
-jac_y = repmat(kron((0:ny - 1)', ones(1, nx)), [1 1 nz]);
-jac_z = repmat(permute(kron(0:nz-1, ones(ny, 1)), [1 3 2]), [1 nx 1]);
+[jac_x, jac_y, jac_z] = meshgrid(0:nx-1, 0:ny-1, 0:nz-1);
 jac_zero = zeros(ny, nx, nz);
 jac_one = ones(ny, nx, nz);
 
