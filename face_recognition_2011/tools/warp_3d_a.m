96c154a8e316e09a01f0bfaa9656b67c0e4a5506
patricksnape <pts08@ic.ac.uk>
2013-01-30 11:03:45 +0000

    Update to use 3D warp

--- face_recognition_2011/tools/warp_3d_a.m
+++ face_recognition_2011/tools/warp_3d_a.m
@@ -6,18 +6,24 @@ function wimg = warp_3d_a(img, p, dst)
 %   DST into IMG.
 %
 %   P = [p1, p3, p5
-%        p2, p4, p6];
+%        p2, p4, p6,
+%        p7, p8, p9];
 
 % Iain Matthews, Simon Baker, Carnegie Mellon University, Pittsburgh
 % $Id: warp_a.m,v 1.1.1.1 2003/08/20 03:07:36 iainm Exp $
 
 if nargin<3 error('Not enough input arguments'); end
-% TODO: fix this
-% Convert affine warp parameters into 3 x 3 warp matrix
-% NB affine parameterised as [1 + p1, p3, p5; p2, 1 + p4, p6]
-M = [p; 0 0 1];
-M(1,1) = M(1,1) + 1;
-M(2,2) = M(2,2) + 1;
+
+% Convert affine warp parameters into 4 x 4 warp matrix
+% NB affine parameterised as [1 -p1 p2 p3
+%                             p4 1 -p5 p6
+%                            -p7 p8 1  p9
+%                             0  0  0  1]
+
+M = [1   -p(1) p(2)  p(3); ...
+     p(4) 1    p(5) -p(6); ...
+    -p(7) p(8) 1     p(9); ...
+     0    0    0     1];
 
 % Use bilinear filtering to warp image back to template
-wimg = quadtobox(img, dst, M, 'bilinear');
+wimg = polytocuboid(img, dst, M);
