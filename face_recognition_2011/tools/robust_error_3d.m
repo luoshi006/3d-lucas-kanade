ad43890a6fadfbf6a0f98f72a71e75e77fe74717
patricksnape <pts08@ic.ac.uk>
2013-02-06 10:55:50 +0000

    Fit IRLS version by replacing robust by Huber M-estimator

--- face_recognition_2011/tools/robust_error_3d.m
+++ face_recognition_2011/tools/robust_error_3d.m
@@ -1,16 +1,14 @@
-function weight = robust_error_3d(error_img, perc_out)
+function weight = robust_error_3d(error_img, k)
 % ROBUST_ERROR - Robust error function
 %  WEIGHT = ROBUST_ERROR (ERROR_IMG, PERC_OUT)
 %
-% Compute robust error function of the error image ERROR_IMG
-% assuming that PERC_OUT percent of the pixels are outliers
+% Compute a Huber M-estimator
 
 % Ralph Gross
 % Carnegie Mellon University, Pittsburgh
 
-[~, ind]       = sort(abs(error_img(:)));
-selInd         = ind(1:round((1 - perc_out) * length(ind)));
-weight         = zeros(size(error_img));
-weight(selInd) = 1;
+weight = zeros(size(error_img));
+weight(abs(error_img) <= k) = 1;
+weight(abs(error_img) > k) = abs(error_img(abs(error_img) > k)) / k;
 
 
