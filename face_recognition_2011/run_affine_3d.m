b09e4ecae980a5afa40ea2bc412fa6f36cb754fd
Patrick Snape <patricksnape@gmail.com>
2013-02-27 15:59:34 +0000

    Edit run-affine 3d so that it can run as a par-for Includes editing the bunny data

--- face_recognition_2011/run_affine_3d.m
+++ face_recognition_2011/run_affine_3d.m
@@ -18,31 +18,32 @@ alg_list = cellfun(@(x) x(1:length(x)-2), alg_list, 'UniformOutput', false);
 verbose = 0;					% Show fitting?
 smoothing = 0;                  % Apply Gaussian smoothing?
 scale = 0.1;                    % % increase in size increase every iteration
-n_iters = 30;					% Number of gradient descent iterations
-n_freq_tests = 50;				% Number of frequency of convergence tests
+n_iters = 10;					% Number of gradient descent iterations
+n_freq_tests = 1;				% Number of frequency of convergence tests
 max_spatial_error = 1.4;        % Max location error for deciding convergence
 
-all_spc_sig = (1:0.5:10);		% All spatial sigmas
+all_spc_sig = (1:10);		    % All spatial sigmas
 
 % Should not need to modify anything below --------------------------------
 
 % tdata - the image and initial template
-load(['data/', data_name])
+tdata = load(['data/', data_name]);
 
 % pt_offset - precomputed random point offsets
-load('data/affine_pt_offset_3d');
+pt_offset = load('data/affine_pt_offset_3d');
+pt_offset = pt_offset.pt_offset;
 num_of_scales = 10;
 results = zeros(num_of_scales, length(all_spc_sig), length(alg_list));
 
 % Run tests
 for iter = 1:num_of_scales
-    fprintf('Iteration %d \n', iter);
     % template img
-    tdata.img2 = tdata.tmplt_img;
+    data.img2 = tdata.tmplt_img;
+    data.tmplt = tdata.tmplt;
 
     s = 1 + ((iter - 1) * scale);
-    [tdata.img1, params] = random_noisy_img(tdata.tmplt_img, tdata.noise_img, s);
-    create_gif(tdata.img1, sprintf('%dx%dx%d - %f (%f).gif', params.w,  params.h,  params.d,  params.ang, s));
+    [data.img1, params] = random_noisy_img(tdata.tmplt_img, tdata.noise_img, s);
+    create_gif(data.img1, sprintf('%dx%dx%d - %f (%f).gif', params.w,  params.h,  params.d,  params.ang, s));
 
     % Matrix S for Gabor-Fourier method, thanx to Peter Kovesi's Gabor Filters, http://www.csse.uwa.edu.au/~pk/
 %         temp = ones(tdata.tmplt(4)-tdata.tmplt(2)+1, tdata.tmplt(3)-tdata.tmplt(1)+1);
@@ -51,19 +52,14 @@ for iter = 1:num_of_scales
 %         save S.mat S;
 
     % Run tests
-    for s=1:length(all_spc_sig)
+    parfor s=1:length(all_spc_sig)
         spatial_sigma = all_spc_sig(s);
 
-        if verbose == 1
-            disp(['DIVG - Spatial: ', num2str(spatial_sigma)]);
-        end
+        fprintf('Scale: %d - Sigma: %.2f \n', iter, spatial_sigma);
 
-        res = test_affine_3d(tdata, pt_offset, alg_list, n_iters, n_freq_tests, spatial_sigma, max_spatial_error, verbose, smoothing);
-        for l = 1:length(alg_list)
-            n_converge = getfield(res, {1}, alg_list{l}, {1}, 'n_converge');
-            results(iter, s, l) = n_converge;
-        end
+        res = test_affine_3d(data, pt_offset, alg_list, n_iters, n_freq_tests, spatial_sigma, max_spatial_error, verbose, smoothing);
+        res = cellfun(@(x) struct2cell(x), struct2cell(res), 'UniformOutput', false);
+        results(iter, s, :) = cell2mat(cellfun(@(x) x{1}, res, 'UniformOutput', false));
     end
+    save(sprintf('results%d.mat', iter), 'results');
 end
-
-save results.mat results;
